34993fc9a1510207d279304b1c2e8b30b95902d8
AlexYaruki <alexyaruki@gmail.com>
2016-11-06 21:10:45 +0100

    Moved all classes inside main LMP class

--- src/main/java/lmp/LMP.java
+++ src/main/java/lmp/LMP.java
@@ -1,26 +1,136 @@
 package lmp;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class LMP {
+public final class LMP {
 
-    public static void parallel(Runnable parallelRegion){
-        if(parallelRegion == null){
+    private LMP(){
+
+    }
+
+    private static class Control {
+        static final int DEFAULT_THREAD_COUNT;
+        static final AtomicInteger threadCount;
+        private static Map<Thread,ParallelContext> contexts;
+        static {
+            DEFAULT_THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+            threadCount = new AtomicInteger(DEFAULT_THREAD_COUNT);
+            contexts = new HashMap<>();
+        }
+
+        public static void removeContextByThread(Thread thread) {
+            contexts.remove(thread);
+        }
+
+        public static void mapContext(Thread thread, ParallelContext context) {
+            contexts.put(thread,context);
+        }
+
+        public static ParallelContext getContext() {
+            return contexts.get(Thread.currentThread());
+        }
+    }
+
+    private static class ParallelContext {
+
+        private int threadCount;
+        private CountDownLatch startupLatch;
+        private Set<Thread> threads;
+
+        public ParallelContext(int threadCount) {
+            this.threadCount = threadCount;
+            startupLatch = new CountDownLatch(threadCount);
+            threads = new HashSet<>();
+        }
+
+        public int getThreadCount() {
+            return threadCount;
+        }
+
+        public void liftStartupLatch() {
+            startupLatch.countDown();
+            try {
+                startupLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void waitOnStartupLatch(){
+            try {
+                startupLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void addThread(Thread thread){
+            threads.add(thread);
+        }
+
+        public void cleanup() {
+            for(Thread thread : threads){
+                Control.removeContextByThread(thread);
+            }
+            threads.clear();
+        }
+    }
+
+    private static class ParallelThreadFactory implements ThreadFactory {
+
+        private ParallelContext context;
+
+        public ParallelThreadFactory(ParallelContext context){
+            this.context = context;
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            Control.mapContext(thread,context);
+            context.addThread(thread);
+            return thread;
+        }
+    }
+
+    private static class ParallelRunner implements Runnable {
+
+        private Runnable parallelRegion;
+        private ParallelContext context;
+
+
+        public ParallelRunner(Runnable parallelRegion,ParallelContext context){
+            this.parallelRegion = parallelRegion;
+            this.context = context;
+        }
+
+        @Override
+        public void run() {
+            context.liftStartupLatch();
+            parallelRegion.run();
+        }
+    }
+
+    public static void parallel(Runnable parallelRegion) {
+        if (parallelRegion == null) {
             throw new NullPointerException("Provided parallel region is empty (Runnable object is null");
         }
-        ParallelContext context = new ParallelContext(LMPControl.getThreadCount());
+        ParallelContext context = new ParallelContext(Control.threadCount.get());
         ParallelThreadFactory threadFactory = new ParallelThreadFactory(context);
-        ExecutorService pool = Executors.newFixedThreadPool(context.getThreadCount(),threadFactory);
-        for(int i = 0; i < context.getThreadCount(); i++){
-            pool.execute(new ParallelRunner(parallelRegion,context));
+        ExecutorService pool = Executors.newFixedThreadPool(context.getThreadCount(), threadFactory);
+        for (int i = 0; i < context.getThreadCount(); i++) {
+            pool.execute(new ParallelRunner(parallelRegion, context));
         }
         context.waitOnStartupLatch();
         pool.shutdown();
         try {
             boolean terminated = false;
-            while(!terminated) {
+            while (!terminated) {
                 terminated = pool.awaitTermination(10, TimeUnit.MILLISECONDS);
             }
         } catch (InterruptedException e) {
@@ -29,15 +139,25 @@ public class LMP {
         context.cleanup();
     }
 
+    public static int getDefaultThreadCount(){
+        return Control.DEFAULT_THREAD_COUNT;
+    }
+
     public static int getThreadCount(){
-        ParallelContext context = LMPControl.getContext();
+        ParallelContext context = Control.getContext();
         if(context == null){
-            return LMPControl.getThreadCount();
+            return Control.threadCount.get();
         } else {
             return context.getThreadCount();
         }
     }
-    public static int getDefaultThreadCount(){
-        return LMPControl.DEFAULT_THREAD_COUNT;
+
+    public static void setThreadCount(final int threadCount) {
+        if(threadCount < 1){
+            Control.threadCount.set(Control.DEFAULT_THREAD_COUNT);
+        } else {
+            Control.threadCount.set(threadCount);
+        }
     }
+
 }
